0ffc10e8322b26517aedae7995d8d108e2db2f97
jimmy.s.huang <jimmy.s.huang@b7c20de4-4b3c-0410-8b16-9d06ef18cdb1>
2007-10-12 15:54:17 +0000

    booking member constraints added admin and member area separated

--- trunk/grails-app/domain/Member.groovy
+++ trunk/grails-app/domain/Member.groovy
@@ -2,8 +2,20 @@ class Member {
 	String name
 	String email
 	String address
+	String password
 	String zipCode
 	String landPhone
 	String mobilePhone
 	String description
+	
+	static hasMany = [bookings:Booking]
+	static mappedBy = [bookings: "member"]
+	static constraints= {
+		password(length:5..20,blank:false)
+    	email(email:true,blank:false,unique:true)
+	}
+	String toString()
+	{
+		return name+"("+email+")"
+	}
 }	
